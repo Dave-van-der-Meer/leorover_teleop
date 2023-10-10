ffdeb947a6f727cca54897458a4771b9cfcebc35
Dave van der Meer <dave.vandermeer@uni.lu>
2023-10-10 13:50:19 +0200

    Make docker_run less system dependent.

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #xhost +local:docker
+LEOROVER='leo01'
 
 # Note: replace --restart=always to --rm for testing
 docker run -it \
@@ -7,10 +8,10 @@ docker run -it \
     --network host \
     --ipc host \
     --privileged \
-    -e ROS_NAMESPACE='leo01' \
-    -e ROS_DOMAIN_ID='0' \
-    --volume="$PWD/../ros2_ws/src/leorover_teleop:/root/ros2_ws/src/leorover_teleop" \
+    -e ROS_NAMESPACE=${ROS_NAMESPACE} \
+    -e ROS_DOMAIN_ID=${ROS_DOMAIN_ID} \
+    --volume="${PWD}/../ros2_ws/src/leorover_teleop:/root/ros2_ws/src/leorover_teleop" \
     --volume="/dev:/dev" \
     --name leorover_teleop \
     local/leorover_teleop:foxy \
-    ros2 launch leorover_teleop joy_teleop_fast.launch.xml cmd_vel_out:=/leo01/cmd_vel
+    ros2 launch leorover_teleop joy_teleop_fast.launch.xml cmd_vel_out:=/${LEOROVER}/cmd_vel
