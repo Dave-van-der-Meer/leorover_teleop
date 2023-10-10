de131eaf080fb8e39cf0583b5066875411e6e3d0
Dave van der Meer <dave.vandermeer@uni.lu>
2023-10-10 14:11:25 +0200

    Upgrade to Humble.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-export ROS2_INSTALL_PATH="/opt/ros/foxy"
+export ROS2_INSTALL_PATH="/opt/ros/humble"
 export ROS_NAMESPACE="leo01"
 export ROS_DOMAIN_ID=0
 
 # setup ros2 environment
 cd /root/ros2_ws
-source "/opt/ros/foxy/setup.bash"
+source "${ROS2_INSTALL_PATH}/setup.bash"
 colcon build --symlink-install
 source "/root/ros2_ws/install/setup.bash"
 
